e856a8b9d786d18de85379ed9cebe53f6ba8290b
Michael G. Noll <michael@confluent.io>
2015-11-20 14:03:44 +0100

    Turn Collector into an abstract class, introduce a Stopped state

--- support-metrics-common/src/main/java/io/confluent/support/metrics/common/Collector.java
+++ support-metrics-common/src/main/java/io/confluent/support/metrics/common/Collector.java
@@ -1,31 +1,29 @@
 /**
  * Copyright 2015 Confluent Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package io.confluent.support.metrics.common;
 
 import org.apache.avro.generic.GenericContainer;
 
-public interface Collector {
+public abstract class Collector {
 
-  enum CollectorState {
+  public enum RuntimeState {
 
-    ShuttingDown(0), Running(1);
+    Stopped(0), ShuttingDown(1), Running(2);
 
     private final int stateId;
 
-    CollectorState(int stateId) {
+    RuntimeState(int stateId) {
       this.stateId = stateId;
     }
 
@@ -35,21 +33,31 @@ public interface Collector {
 
   }
 
+  private RuntimeState runtimeState;
+
+  public Collector() {
+    this.runtimeState = RuntimeState.Stopped;
+  }
+
   /**
    * Collects metrics from a Kafka broker.
+   *
    * @return An Avro record that contains the collected metrics.
    */
-  GenericContainer collectMetrics();
+  public abstract GenericContainer collectMetrics();
 
   /**
-   * Gets the state associated with a Collector
-   * @return Collector State
+   * Gets the runtime state of this collector.
    */
-  CollectorState getState();
+  public RuntimeState getRuntimeState() {
+    return runtimeState;
+  }
 
   /**
-   * Sets the state associated with a Collector
+   * Sets the runtime state of this collector.
    */
-  void setState(CollectorState collectorState);
+  public void setRuntimeState(RuntimeState runtimeState) {
+    this.runtimeState = runtimeState;
+  }
 
 }
